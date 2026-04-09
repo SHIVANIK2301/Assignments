58f7cfa9e4b520cbecc3e49d0261a07a3c85af04
Shivani Kulthe <kultheshivani2301@gmail.com>
2026-04-09 23:09:18 +0530

    Message

---
+++ javascript-basics/data-types/language-basics/debug.js
@@ -0,0 +1,23 @@
+console.log("Executing line 1!");
+console.log("Executing line 2!");
+console.log("Executing line 3!");
+sum(10,50);
+console.log("Executing line 5!");
+console.log("Executing line 6!");
+console.log("Executing line 7!");
+console.log("Executing line 8!");
+console.log("Executing line 9!");
+console.log("Executing line 10!");
+
+
+
+
+
+
+
+function sum(a,b)
+{
+    var c;
+    c=a-b;
+    console.log("SUM:"+c);
+}
